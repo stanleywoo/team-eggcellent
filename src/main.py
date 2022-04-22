c0863cf4ec095726cbe44eec1d6f843c151d16f4
aleksmcgale <aleksandra.mcgale@ssense.com>
2022-04-22 14:27:57 -0400

    feat(Egg-0001): image upload endpoint

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-
+from service.ImageUploadService import upload_image
 app = Flask(__name__)
 # export FLASK_APP=main
 # run flask run to run local server
@@ -7,9 +7,7 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if request.method == 'POST':
-#         return do_the_login()
-#     else:
-#         return show_the_login_form()
+@app.route('/uploadImage', methods=['GET'])
+def uploadImage():
+    if request.method == 'GET':
+        return upload_image('url')
